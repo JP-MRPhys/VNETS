d63f2aa7f2270da0e849b3590e192c65e1eb4bf5
jdp <jehillparikh@gmail.com>
2019-01-29 16:21:12 +0000

    intial commit

--- utils/dicm2nifti.py
+++ utils/dicm2nifti.py
@@ -2,7 +2,6 @@ from nipype.interfaces.dcm2nii import Dcm2niix
 import os
 
 
-
 if __name__ == '__main__':
 
     #example to convert dicom to nifti provide the data contain the dicom folder as source_dir see example below
